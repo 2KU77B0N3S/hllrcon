592ff53f3b857fb9e578b5d9a0e1cf889edc58a4
2KU77B0N3S <thomasfellnerat@gmail.com>
2025-08-03 23:55:47 +0200

    Update constants.py

--- hllrcon/protocol/constants.py
+++ hllrcon/protocol/constants.py
@@ -2,7 +2,7 @@ from typing import Final
 
 DO_ALLOW_CONCURRENT_REQUESTS: Final[bool] = False
 DO_POP_V1_XORKEY: Final[bool] = True
-DO_USE_REQUEST_HEADERS: Final[bool] = False
-DO_XOR_RESPONSES: Final[bool] = False
+DO_USE_REQUEST_HEADERS: Final[bool] = True  # Enable for v2 headers
+DO_XOR_RESPONSES: Final[bool] = True
 
 HEADER_FORMAT: Final[str] = "<II"
