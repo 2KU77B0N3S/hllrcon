9dc2e51bd9ca3bbce8c58782133a7fd6bb22d03c
2KU77B0N3S <thomasfellnerat@gmail.com>
2025-08-03 23:56:07 +0200

    Update request.py

--- hllrcon/protocol/request.py
+++ hllrcon/protocol/request.py
@@ -18,48 +18,37 @@ class RconRequest:
         auth_token: str | None,
         content_body: dict[str, Any] | str = "",
     ) -> None:
-        """Initializes a new RCON request.
-
-        Parameters
-        ----------
-        command : str
-            The command to be executed.
-        version : int
-            The version of the command.
-        auth_token : str | None
-            The authentication token for the RCON connection.
-        content_body : dict[str, Any] | str, optional
-            An additional payload to send along with the command. Must be
-            JSON-serializable.
-
-        """
+        """Initializes a new RCON request."""
         self.name = command
         self.version = version
         self.auth_token = auth_token
         self.content_body = content_body
         self.request_id: int = next(self.__request_id_counter)
 
-    def pack(self) -> bytes:
-        """Packs the request into a bytes object.
-
-        Returns
-        -------
-        bytes
-            The packed request data.
-
-        """
-        body = {
-            "authToken": self.auth_token or "",
-            "version": self.version,
-            "name": self.name,
-            "contentBody": (
+    def pack(self, version: int = 2) -> bytes:
+        """Packs the request into a bytes object."""
+        if version == 2:
+            body = {
+                "AuthToken": self.auth_token or " ",
+                "Version": self.version,
+                "Name": self.name,
+                "ContentBody": (
+                    self.content_body
+                    if isinstance(self.content_body, str)
+                    else json.dumps(self.content_body, separators=(",", ":"))
+                ),
+            }
+            body_encoded = json.dumps(body, separators=(",", ":")).encode()
+            if DO_USE_REQUEST_HEADERS:
+                header = struct.pack(HEADER_FORMAT, self.request_id, len(body_encoded))
+                return header + body_encoded
+            return body_encoded
+        else:
+            content = (
                 self.content_body
                 if isinstance(self.content_body, str)
-                else json.dumps(self.content_body, separators=(",", ":"))
-            ),
-        }
-        body_encoded = json.dumps(body, separators=(",", ":")).encode()
-        if DO_USE_REQUEST_HEADERS:
+                else json.dumps(self.content_body)
+            )
+            body_encoded = (self.name + " " + content if content else self.name).encode()
             header = struct.pack(HEADER_FORMAT, self.request_id, len(body_encoded))
             return header + body_encoded
-        return body_encoded
